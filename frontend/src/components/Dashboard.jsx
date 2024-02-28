b91bd558e47f164679874efaf6dfa1988c90f628
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-02-28 10:11:04 +0700

    Dashboard Responsif (Background)
    Membuat responsif halaman dashboard pada background line wave di about

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,47 +1,59 @@
-import React from 'react'
+import React from "react";
 //HEADING
-import logo from '../assets/biglogo.svg'
-import playstore from '../assets/App Store Black Border.svg'
-import appstore from '../assets/Google Play Black Border.svg'
-import featureee from '../assets/laterrr.svg'
+import logo from "../assets/biglogo.svg";
+import playstore from "../assets/App Store Black Border.svg";
+import appstore from "../assets/Google Play Black Border.svg";
+import featureee from "../assets/laterrr.svg";
 //ABOUT
-import logo3d from '../assets/About/logo3d.svg'
+import logo3d from "../assets/About/logo3d.svg";
+import ombak from "../assets/About/ombak.png";
 const Dashboard = () => {
   return (
     <div>
-        {/* HEADING SECTION */}
-        <div className='flex justify-between w-full bg-primer px-[135px]'>
-            <div>
-                <img src={logo} alt="loading" className='h-[82px]' />
-                <p className='w-[570px] text-[20px] mt-[30px] text-secondary'>
-                    Otonometer menyediakan informasi akurat kepada pengguna indikator keuangan, ekonomi dan statistik dari 549 daerah provinsi, kabupaten dan kota di Indonesia
-                </p>
-                <div className='flex gap-[20px] mt-[30px]'>
-                    <img src={appstore} alt="loading" className='hover:' />
-                    <img src={playstore} alt="loading" className='hover:' />
-                </div>
-            </div>
-            <div>
-                <img src={featureee} alt="" />
-            </div>
+      {/* HEADING SECTION */}
+      <div className="flex justify-between w-full bg-primer px-[135px]">
+        <div>
+          <img src={logo} alt="loading" className="h-[82px]" />
+          <p className="w-[570px] text-[20px] mt-[30px] text-secondary">
+            Otonometer menyediakan informasi akurat kepada pengguna indikator
+            keuangan, ekonomi dan statistik dari 549 daerah provinsi, kabupaten
+            dan kota di Indonesia
+          </p>
+          <div className="flex gap-[20px] mt-[30px]">
+            <img src={appstore} alt="loading" className="hover:" />
+            <img src={playstore} alt="loading" className="hover:" />
+          </div>
         </div>
+        <div>
+          <img src={featureee} alt="" />
+        </div>
+      </div>
 
-        {/* ABOUT SECTION */}
-        <div className='flex justify-center items-center gap-[200px] mt-[128px] px-[205px] h-[531px] bg-ombak'>
-            <img src={logo3d} alt="" className='flex items-center'/>
-            <div>
-                <div className='w-[530px] text-secondary text-[60px] font-bold text-left'>
-                    Tentang Kami
-                </div>
-                <div className='w-[530px] text-secondary text-[16px] text-justify'>
-                Tujuan aplikasi Otonometer ini adalah menyediakan informasi kepada publik luas agar lebih memahami potensi dan kinerja setiap daerah. Kami juga ingin informasi tersebut dapat digunakan untuk penelitian akademis di lembaga riset dan pendidikan. Disamping itu kami berkeinginan agar informasi tersebut menjadi masukan kepada pemerintah daerah dalam mengembangkan kebijakan yang meningkatkan kemandirian daerah. Kami juga ingin membantu pelaku bisnis memahami dan meramalkan tren serta mengidentifikasi peluang investasi dari daerah-daerah tersebut. 
-                </div>
+      {/* ABOUT SECTION */}
+      <div className="relative">
+        <img src={ombak} alt="" className="flex items-center" />
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className="flex justify-center items-center gap-[200px] mt-[10px] px-[205px] h-[531px]">
+            <img src={logo3d} alt="" className="flex items-center" />
+            <div className="absolute w-full mb-[300px] ml-[107%] text-secondary text-[50px] font-bold">
+              Tentang Kami
             </div>
+            <div className="w-full md:w-[950px] mt-[40px] mr-[10px] text-secondary text-[16px] text-justify">
+              Tujuan aplikasi Otonometer ini adalah menyediakan informasi kepada
+              publik luas agar lebih memahami potensi dan kinerja setiap daerah.
+              Kami juga ingin informasi tersebut dapat digunakan untuk
+              penelitian akademis di lembaga riset dan pendidikan. Disamping itu
+              kami berkeinginan agar informasi tersebut menjadi masukan kepada
+              pemerintah daerah dalam mengembangkan kebijakan yang meningkatkan
+              kemandirian daerah. Kami juga ingin membantu pelaku bisnis
+              memahami dan meramalkan tren serta mengidentifikasi peluang
+              investasi dari daerah-daerah tersebut.
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
