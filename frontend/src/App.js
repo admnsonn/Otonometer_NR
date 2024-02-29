7bd008eb7a41e18d30d748f3abd76215a8514bd7
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-02-29 09:35:26 +0700

    Perbaikan dikit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-import Footer from "./components/Footer";
-import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
 import Dashboardpage from "./pages/Dashboardpage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
