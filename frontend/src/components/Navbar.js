492bd20858973d4fa992659cc6c9cabfea342e4e
Rezky Mandar Suaib <rezkymndr@gmail.com>
2024-03-12 02:47:34 +0700

    kondisi navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,48 @@
 import React from 'react'
-import { navLink } from '../data/datanya';
 import logo from "../assets/logonav.svg"
-import { Link } from 'react-scroll';
-
-import {useNavigate} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 
 function Navbar() {
     const navgate = useNavigate()
+    const navLinkStyles = ({isActive}) =>{
+        return{
+            fontWeight: isActive? 'bold' : 'regular'
+        }
+    }
   return (
-    <div className='flex justify-between items-center px-[135px] w-full h-[117px] mb-[30px]'>
+    <nav className='flex justify-between items-center px-[135px] w-full h-[117px] mb-[30px]'>
         <div>
-            <img src={logo} alt="loading" className='w-full h-full object-contain' />
+            <button onClick={() => navgate('/')}>
+                <img src={logo} alt="loading" className='w-full h-full object-contain' />
+            </button>
         </div>
         <ul>
-            <li className='flex gap-x-[25px]'>
-                {navLink.map((item)=>{
+            <li className='flex gap-x-[25px] text-secondary'>
+                {/* {navLink.map((item)=>{
                     return <Link to={item.id} key={item.id} className='text-secondary'>{item.name}</Link>;
-                })}
+                })} */}
+                <NavLink style={navLinkStyles} to={'/'}>
+                    Dashboard
+                </NavLink>
+                <NavLink style={navLinkStyles} to={'/Jelajah'}>
+                    Jelajah
+                </NavLink>
+                <NavLink style={navLinkStyles} to={'/Utak'}>
+                    Utak
+                </NavLink>
+                <NavLink style={navLinkStyles} to={'/Berkaca'}>
+                    Berkaca
+                </NavLink>
+                <p>
+                    Lensa
+                </p>
             </li>
         </ul>
         {/* login-regist button */}
         <div className='flex gap-[10px]'>
             {/* Login */}
             <button 
-            onClick={() => navgate('Login')}
+            onClick={() => navgate('/Login')}
             className='
             flex 
             bg-secondary 
@@ -35,7 +54,7 @@ function Navbar() {
                 Masuk</button>
             {/* Register */}
             <button 
-            onClick={() => navgate('Register')}
+            onClick={() => navgate('/Register')}
             className='
             flex 
             bg-none 
@@ -50,7 +69,7 @@ function Navbar() {
                 Register</button>
         </div>
         
-    </div>
+    </nav>
   );
 }
 
