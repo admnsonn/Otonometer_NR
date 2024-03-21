dc7e45eceba6d4f2e62f95efd024bf7565e2a235
Rezky Mandar Suaib <rezkymndr@gmail.com>
2024-03-21 22:03:35 +0700

    dashboard

---
+++ frontend/src/assets/karaosel.js
@@ -0,0 +1,6 @@
+import image1 from "./jelajah.png"
+import image2 from "./utak.png"
+import image3 from "./berkaca.png"
+import image4 from "./lens.png"
+
+export default [image1,image2,image3,image4];
--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -6,19 +6,50 @@ import Carousel from "../../components/Carousel";
 import logo3d from "../../assets/About/logo3d.svg";
 import ombak from "../../assets/About/ombak.png";
 
+import {motion} from 'framer-motion';
+import { useRef, useEffect, useState } from "react";
+import karaosell from "../../assets/karaosel";
 const Dashboard = () => {
+  
+  function Karousel() {
+  const [width, setWidth] = useState(0);
+  const karousel = useRef();
+
+  useEffect(()=>{
+    setWidth(karousel.current.scrollWidth - karousel.current.offsetWidth);
+  },[]);
+
+    return (
+      <motion.div ref={karousel} className="slidernya" whileTap={{cursor: "grabbing"}}>
+        <motion.div drag="x" dragConstraints={{right : 0, left: -width}} className="inner-karousel">
+          {karaosell.map(slideer =>{
+            return(
+              <motion.div className="item" key={slideer}>
+                <img src={slideer} alt="slider"/>
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    );
+  }
+  
   return (
-    <div className="">
+    <section>
       {/* HEADING SECTION */}
-      <div className="flex xl:absolute xl:justify-between w-full bg-primer xl:pl-[10%] px-[15%] xl:gap-[20px] mt-[200px]">
+      <div className="text-[14px] text-center text-secondary mt-[50px] w-[260px] mx-auto xl:hidden">Layanan untuk mengenal lebih jauh <span className="font-bold">(keotonomian fiskal)</span> daerah Anda!</div>
+      <div className="w-[300px] mx-auto xl:hidden mt-[150px] mb-[100px]">
+        <Karousel/>
+      </div>
+      <div className="flex xl:absolute xl:justify-between w-full bg-primer xl:pl-[10%] px-[15%] xl:gap-[20px] xl:mt-[200px] mt-[50px]">
         <div className="flex flex-col justify-center items-start w-[800px] xl:w-full">
-          <img src={logo} alt="loading" className="h-[60px] xl:h-[82px]" />
-          <p className="w-full xl:w-[900px] text-[20px] mt-[30px] text-secondary">
-            Otonometer menyediakan informasi akurat kepada pengguna indikator
+          {/* <img src={logo} alt="loading" className="h-[0] xl:h-[82px]"/> */}
+          <p className="w-full xl:w-[900px] text-[14px] xl:text-[20px] mt-[10px] text-secondary text-center xl:text-left">
+            <span className="font-bold">Otonometer</span> menyediakan informasi akurat kepada pengguna indikator
             keuangan, ekonomi dan statistik dari 549 daerah provinsi, kabupaten
             dan kota di Indonesia
           </p>
-          <div className="flex gap-[20px] mt-[30px]">
+          <div className="flex gap-[20px] mt-[30px] mx-auto xl:mx-0">
             <img src={appstore} alt="loading" className="hover:" />
             <img src={playstore} alt="loading" className="hover:" />
           </div>
@@ -28,6 +59,7 @@ const Dashboard = () => {
       <div className="hidden xl:block left-0 right-0">
           <Carousel/>
       </div>
+      
       {/* ABOUT SECTION */}
       <div className="xl:relative xl:mt-0 mt-[200px]">
         <img src={ombak} alt="illustrate" className="hidden xl:flex items-center" />
@@ -53,8 +85,10 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
-
+<style>
+  
+</style>
 export default Dashboard
